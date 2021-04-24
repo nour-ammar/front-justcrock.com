a947131ce1495a042a9b5525f0c9786061287bc7
nouha jbeli <jbelinouha9@gmail.com>
2021-04-24 14:44:11 +0200

    commentaire front

--- src/app/services/recette.service.ts
+++ src/app/services/recette.service.ts
@@ -41,4 +41,29 @@ export class RecetteService {
    getrecetteById(id: string) {
     return this.http.get('http://localhost:3000/api/recette/' + id);
   }
+  getServiceComments(id:any){
+    return this.http.get('http://localhost:3000/api/comment/' + id);
+
+  }
+  addServiceComment(idUser:any,comment:any,idrecette:any){
+    const body={ commentaire:comment, UserId:idUser,Id_recette:idrecette}
+    return this.http.post('http://localhost:3000/api/comment/addComment',body);
+
+  }
+  deleteServiceComment(id:any){
+    return this.http.delete('http://localhost:3000/api/comment/' + id);
+
+  }
+  editServiceComment(commentaire:any,id:any,idrecette:any,userId:any){
+   const body={
+    commentaire:commentaire,
+    id:id,
+    Id_recette:idrecette,
+    UserId:userId
+    }
+
+    return this.http.put('http://localhost:3000/api/comment/editComment',body);
+
+
+  }
 }

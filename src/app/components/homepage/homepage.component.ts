4eb725c10cb1ef64b288b5bf64a6c5d77ac78677
noor <nooraammar19@gmail.com>
2021-04-29 08:30:35 +0200

    last updates

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
-
-  constructor() { }
+  constructor(private titleService: Title, private metaTagService: Meta) {}
 
   ngOnInit(): void {
+    this.titleService.setTitle('homepage');
+    this.metaTagService.addTag({
+      name: 'keywords',
+      content: 'recttes recipe halal islam food ',
+    });
+    this.metaTagService.updateTag({
+      name: 'description',
+      content: 'recettes halal - food Halal',
+    });
   }
-
 }

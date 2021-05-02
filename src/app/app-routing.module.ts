3f36906008513efb8c2e52803d5463d1432e1b1d
nouha jbeli <jbelinouha9@gmail.com>
2021-05-02 16:12:13 +0200

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,7 +35,7 @@ const routes: Routes = [
   {path:'addAvis',component:AddAvisComponent},
   {path:'editAvis/:id',component:EditAvisComponent},
   {path:'connect',component:LoginComponent},
-
+// commentaire
 ];
 
 @NgModule({
